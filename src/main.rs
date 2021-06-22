12e8ef1e067c68f9d86b942f6e01d9199cf8b0f0
Aleksey Khivrenko <khivrenko@gmail.com>
2021-06-21 20:04:17 -0700

    identify issues

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use std::env;
 use std::fs;
 
+//TODO: resolve issues
+//  1. main has too many resposibilities (well 2 accept args and read file)
+//  2. group configuration variables into 1 structure to make their purpose clearer
+//  3. properly handle file errors instead of just using 'expect'
+//  4. consolidate all error handling code into 1 place
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
@@ -18,4 +23,8 @@ fn main() {
 
         println!("File Contents\n{}", contents);
     }
+}
+
+fn get_args() {
+
 }
