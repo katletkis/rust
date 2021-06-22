a7fdacc53ef54cea6537d01b40bec699120349e7
Aleksey Khivrenko <khivrenko@gmail.com>
2021-06-21 22:59:06 -0700

    use Test Driven Design (TDD) to implement minigrep search function. First, write one_result() test and then implement the search function that the one_result() test uses. Finally, use the search funciton in the run function so that the program works.

--- src/lib.rs
+++ src/lib.rs
@@ -22,15 +22,37 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("File Contents\n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
 
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result: Vec<&str> = vec![];
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line.trim());
+        }
+    }
+    result
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+        Rust:
+        safe, fast, productive
+        Pick three.";
+        assert_eq!(vec!["safe, fast, productive"], search(query, contents));
+    }
+
     #[test]
     fn args() {
         let args: Vec<String> = vec!["my_prog".to_string(), "foo".to_string(), "bar".to_string()];
--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Searching for '{}' in file '{}'", config.query, config.filename);
+    // println!("Searching for '{}' in file '{}'", config.query, config.filename);
 
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
